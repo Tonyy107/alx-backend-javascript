bdf68bdc655065fef5becfaab1f137c9941a401b
Tonyy107 <antonmagdy53@gmail.com>
2024-10-31 18:11:30 +0300

    Add tests for handleResponseFromAPI and update promise handling

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,17 +1,17 @@
+// 2-then.js
 export default function handleResponseFromAPI(promise) {
-  const answer = new Promise((myResolve, myReject) => {
+  return new Promise((myResolve, myReject) => {
     if (promise) {
       myResolve({
         body: 'success',
-        status: 200
+        status: 200,
       });
     } else {
       myReject(new Error());
     }
+  }).then(() => {
+    console.warn('Got a response from the API');
+  }).catch(() => {
+    console.warn('Got a response from the API');
   });
-  return answer;
 }
-
-handleResponseFromAPI(true).then(() => {
-  console.log('Got a response from the API');
-});

cd3428636ccdb6ba2b25e58d90191b7310cc01d1
Tonyy107 <antonmagdy53@gmail.com>
2024-10-31 19:35:46 +0300

    Return the result of Promise.all in handleProfileSignup for better promise chaining

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -4,7 +4,7 @@ export default function handleProfileSignup() {
   const promise1 = uploadPhoto();
   const promise2 = createUser();
 
-  Promise.all([promise1, promise2]).then((res) => {
+  return Promise.all([promise1, promise2]).then((res) => {
     const [{ body: x },
       { firstName: y, lastName: z }] = res;
     console.log(x, y, z);

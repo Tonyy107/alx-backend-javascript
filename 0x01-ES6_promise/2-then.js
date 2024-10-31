76946c069672ea577d2bdeb2fe2e0a5578fa2dc2
Tonyy107 <antonmagdy53@gmail.com>
2024-10-31 18:33:47 +0300

    Refactor handleResponseFromAPI to simplify promise handling and improve logging

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,19 +1,6 @@
-// 2-then.js
 export default function handleResponseFromAPI(promise) {
-  return new Promise((myResolve, myReject) => {
-    if (promise) {
-      myResolve(
-        {
-          body: 'success',
-          status: 200
-        }
-      );
-    } else {
-      myReject(new Error());
-    }
-  }).then(() => {
-    console.warn('Got a response from the API');
-  }).catch(() => {
-    console.warn('Got a response from the API');
-  });
+  return promise
+    .then(() => ({ status: 200, body: 'success' }))
+    .catch(() => Error())
+    .finally(() => { console.log('Got a response from the API'); });
 }
